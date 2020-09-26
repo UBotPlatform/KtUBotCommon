d274b7bbc2e75b01f15315067fb3238158ce4e5b
qiqiworld <1354092549@qq.com>
2020-09-26 23:09:28 +0800

    fix: using properly mark about json-rpc protocol

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
-    api("com.github.1354092549:ktjsonrpcpeer:v0.3.2")
+    api("com.github.1354092549:ktjsonrpcpeer:v0.3.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
@@ -21,7 +21,7 @@ configure<PublishingExtension> {
         create<MavenPublication>("maven") {
             groupId = "ubot"
             artifactId = "common"
-            version = "0.4.1"
+            version = "0.4.2"
             from(components["java"])
         }
     }
